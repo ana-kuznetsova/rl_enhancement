63b3c0464eda6207387456c547139d6cce68b45c
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-07-14 17:19:20 -0400

    fix args

--- 2017/models.py
+++ 2017/models.py
@@ -158,7 +158,7 @@ def inference(test_data_path, clean_test_path, out_test, model_path,
              win_len=512, hop_size=256, fs=44000):
     model = DNN()
     model.load_state_dict(torch.load(model_path+'dnn_map_best.pth'))
-    testData = data.DataLoader(testDataLoader())
+    testData = data.DataLoader(testDataLoader(test_data_path))
 
     names = testData.test_files
 
