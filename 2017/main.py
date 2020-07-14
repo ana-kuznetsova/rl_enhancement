e739aa42bc1c37a43b2975b157c413bdb211d4ea
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-07-14 12:04:09 -0400

    add testing to main

--- 2017/main.py
+++ 2017/main.py
@@ -5,27 +5,35 @@ import argparse
 
 from models import train_dnn
 from models import pretrain
+from models import inference
 
 
 def main(args):
-    print('Start DNN mapping...')
-    train_dnn(args.num_epochs,
-              args.model_path,
-              args.x_path,
-              args.y_path,
-              args.loss_path)
+    if args.mode=='train':
+        print('Start DNN mapping...')
+        train_dnn(args.num_epochs,
+                args.model_path,
+                args.x_path,
+                args.y_path,
+                args.loss_path)
+    elif args.mode=='test':
+        print('Staring inference on test data...')
+        inference(args.test_path, args.test_out)
 
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('--num_epochs', type=int, help='Number of epochs', required=True)
+    parser.add_argument('--num_epochs', type=int, help='Number of epochs')
     parser.add_argument('--chunk_size', type=int, help='Number of training examples to load from disk')
     parser.add_argument('--model_path', type=str, help='Dir to save best model', required=True)
-    parser.add_argument('--x_path', type=str, help='path to X files', required=True)
-    parser.add_argument('--y_path', type=str, help='path to y files', required=True)
-    parser.add_argument('--loss_path', type=str, help='Dir to save losses', required=True)
+    parser.add_argument('--x_path', type=str, help='path to X files')
+    parser.add_argument('--y_path', type=str, help='path to y files')
+    parser.add_argument('--loss_path', type=str, help='Dir to save losses')
     parser.add_argument('--pretrained', type=str, help='True or false, load weights from pretrained model')
+    parser.add_argument('--mode', type=str, help='Train or test', required=True)
+    parser.add_argument('--test_path', type=str, help='path to test data')
+    parser.add_argument('--test_out', type=str, help='Path to dir to save test output')
 
 
     args = parser.parse_args()
--- 2017/models.py
+++ 2017/models.py
@@ -153,7 +153,7 @@ def pretrain(num_epochs, model_path, x_path, y_path, weights_path,
 
 
 def inference(test_data_path, out_test,
-             win_len, hop_size, fs):
+             win_len=512, hop_size=256, fs=44000):
     model = DNN()
     model.load_state_dict(torch.load(model_path+'dnn_map_best.pth'))
     testData = data.DataLoader(testDataLoader())
