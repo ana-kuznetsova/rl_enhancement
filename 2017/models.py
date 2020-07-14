2a42c912e0939889609845508c382df86bb24b77
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-07-14 18:21:53 -0400

    del orint

--- 2017/models.py
+++ 2017/models.py
@@ -175,7 +175,7 @@ def inference(test_data_path, clean_test_path, out_test, model_path, maxlen=1399
         with torch.no_grad():
             output = model(audio)
             output = np.transpose(output[0].cpu().data.numpy().squeeze())
-            print(output, output.shape)
+            #print(output, output.shape)
         #output = librosa.istft(np.transpose(output[0].cpu().data.numpy().squeeze()), hop_length=hop_size,
         #                    win_length=win_len) 
         #    librosa.output.write_wav(out_test+name, output, fs) 
