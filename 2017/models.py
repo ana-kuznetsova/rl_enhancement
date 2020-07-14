a5635c8129bdb8acc5eb6bca5487a9a8cf3259e5
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-07-14 18:23:22 -0400

    del orint

--- 2017/models.py
+++ 2017/models.py
@@ -168,7 +168,7 @@ def inference(test_data_path, clean_test_path, out_test, model_path, maxlen=1399
 
 
     for step, audio in enumerate(testData):
-        print('Step:', step)
+       # print('Step:', step)
 
         name = names[step].split('.')[0]
         name = name+'.wav' 
