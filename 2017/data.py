18cd794fbb93404ed695a69761b463ab55e54200
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-07-16 11:54:32 -0400

    remove padding

--- 2017/data.py
+++ 2017/data.py
@@ -147,9 +147,8 @@ def make_batch(x_path, y_path, ind, P, maxlen, win_len, hop_size, fs):
 
     for path in tqdm(chunk_x):
         arr = np.load(x_path+path)
-        arr = np.abs(get_X_batch(arr, P)).T
-        arr = pad(arr, maxlen)
-        X.extend(arr.T)
+        arr = np.abs(get_X_batch(arr, P))
+        X.extend(arr)
 
         arr = np.load(y_path+path)
         arr = np.abs(pad(arr, maxlen))
