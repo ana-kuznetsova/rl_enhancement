fd53f6f051e450accf5a9a0df03ddb82ca35f228
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-07-16 13:51:06 -0400

    debug

--- 2017/models.py
+++ 2017/models.py
@@ -180,6 +180,7 @@ def inference(test_data_path, clean_test_path, out_test, model_path, chunk_size,
         testData = data.DataLoader(testDataLoader(X_chunk), batch_size = 1339)
 
         chunk_names = fnames[start:end]
+        print(chunk_names)
         for step, audio in enumerate(testData):
             #print('Step:', step)
 
