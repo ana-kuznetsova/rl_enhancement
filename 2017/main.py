1869af8f0432ee2bd7575b1c1c43fb198c8c1cf3
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-07-16 13:11:57 -0400

    change arg

--- 2017/main.py
+++ 2017/main.py
@@ -19,7 +19,7 @@ def main(args):
                 args.chunk_size)
     elif args.mode=='test':
         print('Staring inference on test data...')
-        inference(args.test_path, args.clean_test_path, args.test_out, args.model_path)
+        inference(args.test_path, args.clean_test_path, args.test_out, args.model_path, args.chunk_size)
 
 
 
