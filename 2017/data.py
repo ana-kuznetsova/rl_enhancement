be4efd00d7554e91f0f01a196443267d259dcadb
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-07-19 01:28:53 -0400

    remove logs

--- 2017/data.py
+++ 2017/data.py
@@ -156,7 +156,7 @@ def make_batch(x_path, y_path, ind, P, maxlen, win_len, hop_size, feat_type, fs)
             arr = np.abs(get_X_batch(arr, P))
         X.extend(arr)
 
-        arr = np.log(np.load(y_path+path))
+        arr = np.load(y_path+path)
         arr = np.abs(pad(arr, maxlen)).T
         y.extend(arr)
     X = np.asarray(X)
