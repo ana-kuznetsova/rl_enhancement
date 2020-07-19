f0950075179e450b889f9a15439fb75d0dc7d6f8
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-07-18 22:01:17 -0400

    fix log

--- 2017/data.py
+++ 2017/data.py
@@ -146,8 +146,8 @@ def make_batch(x_path, y_path, ind, P, maxlen, win_len, hop_size, fs):
     chunk_x = os.listdir(x_path)[ind[0]:ind[1]]
 
     for path in tqdm(chunk_x):
-        arr = np.nan_to_num(np.load(x_path+path))
-        arr = 10*np.log10(pad(arr, maxlen))
+        arr = 10*np.log10(np.nan_to_num(np.load(x_path+path)))
+        arr = pad(arr, maxlen)
         arr = np.abs(get_X_batch(arr, P))
         X.extend(arr)
 
