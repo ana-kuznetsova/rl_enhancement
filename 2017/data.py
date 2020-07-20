5dca71fc126c738f1a6fd8672fabc272a5aa7c74
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-07-20 13:38:50 -0400

    pad

--- 2017/data.py
+++ 2017/data.py
@@ -160,7 +160,7 @@ def make_batch(x_path, y_path, ind, P, maxlen, win_len, hop_size, feat_type, fs)
             arr = np.abs(get_X_batch(arr, P))
         X.extend(arr)
 
-        arr = np.abs(np.load(y_path+path))
+        arr = pad(np.abs(np.load(y_path+path)), maxlen)
         print('y shape:', arr.shape)
         #predict log of speech
         y.extend(arr)
