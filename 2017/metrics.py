99b461004e32b90c6fd4c17a3ee964242e7eaf9a
Nastya <nastya@Nikolais-MacBook-Air.local>
2020-07-24 15:14:14 -0400

    fix resq

--- 2017/metrics.py
+++ 2017/metrics.py
@@ -13,14 +13,15 @@ def eval_pesq(noisy_test, clean_test, out_path, fs=16000):
 
     print('Calculating PESQ...')
     for f in tqdm(clean):
-        reference, sr = librosa.load(clean_test+f, mono=True)
-        reference = librosa.core.resample(reference, sr, 16000)
-        ind = noisy.index(f)
-        degraded = librosa.istft(noisy_test+noisy[ind], hop_length=256, win_length=512)
-        degraded = degraded[:reference.shape[0]]
-        score = pypesq(reference, degraded, fs)
-        print('Test file:', f, 'PESQ: ', score)
-        scores.append(score)
+        if '.wav' in f:
+            reference, sr = librosa.load(clean_test+f, mono=True)
+            reference = librosa.core.resample(reference, sr, 16000)
+            ind = noisy.index(f)
+            degraded = librosa.istft(noisy_test+noisy[ind], hop_length=256, win_length=512)
+            degraded = degraded[:reference.shape[0]]
+            score = pypesq(reference, degraded, fs)
+            print('Test file:', f, 'PESQ: ', score)
+            scores.append(score)
 
     df = pd.DataFrame([clean, scores], columns=['fname', 'PESQ'])
     df.to_csv(out_path+'PESQ.csv')
