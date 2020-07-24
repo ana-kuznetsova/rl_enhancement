92118d95d622fa64bf7814dae65f2189c08eba42
Nastya <nastya@Nikolais-MacBook-Air.local>
2020-07-24 15:29:35 -0400

    fix pesq

--- 2017/metrics.py
+++ 2017/metrics.py
@@ -5,7 +5,10 @@ import os
 import soundfile as sf
 import librosa
 
-def eval_pesq(noisy_test, clean_test, out_path, fs=16000):
+def eval_pesq(noisy_test, clean_test, out_path, 
+    img_path='/N/slate/anakuzne/se_data/snr0_test_img/', 
+    fs=16000):
+
     noisy = os.listdir(noisy_test)
     clean = os.listdir(clean_test)
 
@@ -17,7 +20,10 @@ def eval_pesq(noisy_test, clean_test, out_path, fs=16000):
             reference, sr = librosa.load(clean_test+f, mono=True)
             reference = librosa.core.resample(reference, sr, 16000)
             ind = noisy.index(f)
-            degraded = librosa.istft(noisy_test+noisy[ind], hop_length=256, win_length=512)
+            degraded = np.load(noisy_test+noisy[ind])
+            imag = np.load(img_path+noisy[ind])
+            degraded = degraded + imag
+            degraded = librosa.istft(degraded, hop_length=256, win_length=512)
             degraded = degraded[:reference.shape[0]]
             score = pypesq(reference, degraded, fs)
             print('Test file:', f, 'PESQ: ', score)
