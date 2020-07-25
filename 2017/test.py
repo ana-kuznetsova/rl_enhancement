de69ffd334b2436e34c9eb3ea0ae3c4a066872e6
Nastya <nastya@Nikolais-MacBook-Air.local>
2020-07-25 13:22:36 -0400

    pretrain test

---
+++ 2017/test.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn.functional as Func
+import torch.nn as nn
+import numpy as np
+from torch.autograd import Variable
+import torch.optim as optim
+import torch.utils.data as data
+import copy
+
+from models import Layer1
+from models import Layer_1_2
+
+pretrain_path = '/N/slate/anakuzne/se_out/pretrain/dnn_l1.pth'
+
+l1 = Layer1()
+l1.load_state_dict(torch.load(model_path+'dnn_l1.pth'))
+
+print(l1.fc1.children())
+
