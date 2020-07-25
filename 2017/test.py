1adf09ef20408f3b7b4dbc4bda24cbf7b459fcca
Nastya <nastya@Nikolais-MacBook-Air.local>
2020-07-25 13:34:25 -0400

    test

--- 2017/test.py
+++ 2017/test.py
@@ -15,5 +15,13 @@ pretrain_path = '/N/slate/anakuzne/se_out/pretrain/dnn_l1.pth'
 l1 = Layer1()
 l1.load_state_dict(torch.load(pretrain_path))
 
-newmodel = torch.nn.Sequential(*(list(l1.children())[:-1]))
-print(newmodel)
+newmodel = torch.nn.Sequential(*(list(l1.children())[:-2]))
+
+l2 = Layer_1_2(newmodel)
+criterion = nn.MSELoss()
+optimizer = optim.SGD(l2.parameters(), lr=0.01, momentum=0.9)
+device = torch.device("cuda")
+l2.cuda()
+l2 = l2.to(device)
+criterion.cuda()
+best_l2 = copy.deepcopy(l2.state_dict())
