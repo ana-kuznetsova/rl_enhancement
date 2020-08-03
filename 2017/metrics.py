21da6fab1b3b76ecc527615e137d5d9db0aa9d33
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-08-03 12:34:46 -0400

    debug metrics

--- 2017/metrics.py
+++ 2017/metrics.py
@@ -8,7 +8,7 @@ import numpy as np
 from data import pad
 
 def eval_pesq(noisy_test, clean_test, out_path, 
-    img_path='/N/slate/anakuzne/se_data/snr0_test_img/', 
+    img_path='/u/anakuzne/data/snr0_test_img/', 
     fs=16000):
 
     noisy = os.listdir(noisy_test)
@@ -19,8 +19,8 @@ def eval_pesq(noisy_test, clean_test, out_path,
 
     print('Calculating PESQ clean refernence...')
     for f in tqdm(clean):
+        print('F:', f)
         if '.wav' in f:
-            print('F:', f)
             reference, sr = librosa.load(clean_test+f, mono=True)
             reference = librosa.core.resample(reference, sr, 16000)
             print('FNAME:', p.split('/')[-2]+ '_' + p.split('/')[-1].split('.')[0] + '.npy')
