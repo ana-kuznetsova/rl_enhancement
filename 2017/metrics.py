0fb209dc9319d0302a0ef0d74e107cb3e2ed32f9
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-08-03 12:38:39 -0400

    debug metrics

--- 2017/metrics.py
+++ 2017/metrics.py
@@ -20,23 +20,21 @@ def eval_pesq(noisy_test, clean_test, out_path,
 
     print('Calculating PESQ clean refernence...')
     for f in tqdm(clean):
-        print('F:', f)
-        if '.wav' in f:
-            reference, sr = librosa.load(clean_test+f, mono=True)
-            reference = librosa.core.resample(reference, sr, 16000)
-            print('FNAME:', p.split('/')[-2]+ '_' + p.split('/')[-1].split('.')[0] + '.npy')
-            ind = noisy.index(p.split('/')[-2]+ '_' + p.split('/')[-1].split('.')[0] + '.npy')
-            degraded = np.load(noisy_test+noisy[ind])
+        reference, sr = librosa.load(f, mono=True)
+        reference = librosa.core.resample(reference, sr, 16000)
+        print('FNAME:', p.split('/')[-2]+ '_' + p.split('/')[-1].split('.')[0] + '.npy')
+        ind = noisy.index(p.split('/')[-2]+ '_' + p.split('/')[-1].split('.')[0] + '.npy')
+        degraded = np.load(noisy_test+noisy[ind])
             
-            ind = imag.index('corpus_'+ f.split('.')[0]+'.npy')
-            imag_num = pad(np.load(img_path+noisy[ind]), 1339)
-            degraded = degraded + imag_num
-            degraded = librosa.istft(degraded, hop_length=256, win_length=512)
-            degraded = degraded[:reference.shape[0]]
-            #print('degraded:', degraded.shape)
-            score = pesq(reference, degraded, fs)
-            #print('Test file:', f, 'PESQ: ', score)
-            scores.append(score)
+        ind = imag.index('corpus_'+ f.split('.')[0]+'.npy')
+        imag_num = pad(np.load(img_path+noisy[ind]), 1339)
+        degraded = degraded + imag_num
+        degraded = librosa.istft(degraded, hop_length=256, win_length=512)
+        degraded = degraded[:reference.shape[0]]
+        #print('degraded:', degraded.shape)
+        score = pesq(reference, degraded, fs)
+        #print('Test file:', f, 'PESQ: ', score)
+        scores.append(score)
     clean = [n for n in clean if '.wav' in n]
     data = {'fname':clean, 'PESQ_clean':scores}
     df = pd.DataFrame(data, columns=['fname', 'PESQ_clean_ref', 'PESQ_noisy_ref'])
