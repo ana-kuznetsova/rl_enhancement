3439376f655d9209230b8015809e0bf409b6a038
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-08-05 19:35:04 -0400

    bnorm

--- 2017/models.py
+++ 2017/models.py
@@ -64,10 +64,12 @@ class Layer1(nn.Module):
     def __init__(self):
         super().__init__()
         self.fc1 = nn.Linear(704, 128)
+        self.bnorm = nn.BatchNorm1d(704)
         self.drop = nn.Dropout(0.3)
         self.out = nn.Linear(128, 257)
 
     def forward(self, x):
+        x = self.bnorm(x)
         x = Func.sigmoid(self.fc1(x))
         x = self.drop(x)
         return self.out(x)
@@ -80,9 +82,11 @@ class Layer_1_2(nn.Module):
         self.fc1 = nn.Linear(704, 128)
         self.fc2 = nn.Linear(128, 128)
         self.drop = nn.Dropout(0.3)
+        self.bnorm = nn.BatchNorm1d(704)
         self.out = nn.Linear(128, 257)
 
     def forward(self, x):
+        x = self.bnorm(x)
         x = Func.sigmoid(self.fc1(x))
         x = Func.sigmoid(self.fc2(x))
         x = self.drop(x)
