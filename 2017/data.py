a35e6c47755c63eecf4a4de1ee239c876669958c
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-08-08 20:24:00 -0400

    debug

--- 2017/data.py
+++ 2017/data.py
@@ -225,7 +225,7 @@ def KMeans(chunk_size, train_path, out_path):
         end = min(start+chunk_size, 4620)
         print(start, end)
         X = get_freq_bins(paths, [start, end])
-        print('Shape:', X.shape)
+        #print('Shape:', X.shape)
         kmeans = kmeans.partial_fit(np.abs(X))
 
     centers = kmeans.cluster_centers_
