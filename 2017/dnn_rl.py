4b55fb0363fa619b643e12add2dbe3cdf5737c37
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-08-10 19:04:04 -0400

    test

--- 2017/dnn_rl.py
+++ 2017/dnn_rl.py
@@ -83,7 +83,7 @@ def q_learning(x_path, y_path, model_path,
     ###Load DNN-mapping model
     dnn_map = DNN_mel()
     dnn_map.load_state_dict(torch.load(model_path+'dnn_map_best.pth'))
-    dnn_map = dnn_map.todevice("cuda:0")
+    dnn_map = dnn_map.to("cuda:0")
 
     #criterion = nn.MSELoss()
     #optimizer = optim.SGD(dnn_rl.parameters(), lr=0.01, momentum=0.9)
