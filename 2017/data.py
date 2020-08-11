9aa33ec49b3195c029c17c029183f1653bfb48c5
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-08-10 21:17:09 -0400

    syntax

--- 2017/data.py
+++ 2017/data.py
@@ -123,7 +123,7 @@ def calc_masks(speech_paths, noise_path, fs, win_len, hop_size,
             target = np.log(stft_clean) + 0.0000001
             #target = np.nan_to_num(target)
             write_npy(mask_dir, fname, target)
-        elif mask_type='stft':
+        elif mask_type=='stft':
             write_npy(stft_dir, fname, stft_clean)
 
 
