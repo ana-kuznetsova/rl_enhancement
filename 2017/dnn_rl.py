55946048d351ec89b0957d0b8e2ba4c9ff73ce90
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-08-11 14:33:51 -0400

    debug

--- 2017/dnn_rl.py
+++ 2017/dnn_rl.py
@@ -113,6 +113,8 @@ def q_learning(x_path, y_path, model_path, clean_path,
     ####### PREDICT DNN-RL AND DNN-MAPPING OUTPUT #######
     rl_out = dnn_rl(x)
     wiener_rl = np.zeros((1339, 257))
+
+    print("OUT:", rl_out)
     
     #Select template index, predict Wiener filter
     for i, row in enumerate(rl_out):
