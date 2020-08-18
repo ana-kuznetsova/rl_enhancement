3eb09578e15f1f7bb053493e10d1be4040705dbd
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-08-18 12:09:24 -0400

    debug

--- 2017/dnn_rl.py
+++ 2017/dnn_rl.py
@@ -41,6 +41,7 @@ def time_weight(Y, S):
     '''
     Y = np.nan_to_num(np.log(np.abs(Y)))
     S = np.nan_to_num(np.log(np.abs(S)))
+    print((Y - S)**2)
     E_approx = np.sum((Y - S)**2, axis=0)
     print('E approx:', E_approx)
     E = E_approx/np.max(E_approx)
