d4c36280711ed3fbace16ef1b74062e291189702
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-08-20 14:16:59 -0400

    test

--- 2017/dnn_rl.py
+++ 2017/dnn_rl.py
@@ -189,8 +189,9 @@ def MMSE_pretrain(chunk_size, x_path, y_path, model_path, cluster_path,
                     wiener_rl[i] = G_k_pred
 
                 wiener_rl = wiener_rl.T
+                print('Phase:', phase.shape)
 
-                y_pred_rl = np.multiply(audio.detach().cpu().numpy(), wiener_rl) + phase
+                y_pred_rl = np.multiply(audio.detach().cpu().numpy().T, wiener_rl) + phase
                 y_pred_rl = torch.tensor(y_pred_rl, requires_grad=True).cuda().float()
 
                 clean = pad(np.load(clean_path+fnames[step]), maxlen)
