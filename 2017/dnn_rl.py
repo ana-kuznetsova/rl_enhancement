26ce10526f4e518c4ed2fb66750cf05162ef887a
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-08-20 16:00:09 -0400

    test

--- 2017/dnn_rl.py
+++ 2017/dnn_rl.py
@@ -54,11 +54,12 @@ class RL_L2(nn.Module):
         x = self.drop(x)
         return self.soft(x)
 
-def q_training_step(output, step, x_path, clean_path, imag_path, fnames, maxlen=1339):
+def q_training_step(output, step, G, x_path, clean_path, imag_path, fnames, maxlen=1339):
     '''
     Params:
         output: NN predictions
         step: step index
+        G: cluster centers
         x_path
         clean_path
         imag_path
@@ -205,7 +206,7 @@ def MMSE_pretrain(chunk_size, x_path, y_path, model_path, cluster_path,
                 target = target.to(device)
                 output = l1(audio)
                 
-                newLoss = q_training_step(output, step, x_path, clean_path, imag_path, fnames)               
+                newLoss = q_training_step(output, step, G, x_path, clean_path, imag_path, fnames)               
                 chunk_loss += newLoss.data
                 optimizer.zero_grad()
                 newLoss.backward()
