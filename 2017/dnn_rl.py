92e7a1d87ad393e021cf8452eb4643fbb2187e47
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-08-20 15:54:13 -0400

    test

--- 2017/dnn_rl.py
+++ 2017/dnn_rl.py
@@ -54,6 +54,37 @@ class RL_L2(nn.Module):
         x = self.drop(x)
         return self.soft(x)
 
+def q_training_step(output, x_path, clean_path, imag_path, fnames):
+    '''
+    Params:
+        output: NN predictions
+        x_path
+        clean_path
+        imag_path
+        fnames
+    '''
+    phase = pad(np.load(imag_path+fnames[step]), maxlen).T
+    Q_pred = output.detach().cpu().numpy()
+    wiener_rl = np.zeros((1339, 257))
+
+    #Select template index, predict Wiener filter
+    for i, row in enumerate(Q_pred):
+        ind = np.argmax(row)
+        G_k_pred = G[ind]
+        wiener_rl[i] = G_k_pred
+
+    x_source = np.load(x_path+fnames[step])
+    x_source = pad(x_source, maxlen).T
+                
+    y_pred_rl = np.multiply(x_source, wiener_rl) + phase
+    y_pred_rl = torch.tensor(y_pred_rl, requires_grad=True).cuda().float()
+
+    clean = pad(np.load(clean_path+fnames[step]), maxlen).T
+    clean = torch.tensor(clean).cuda().float()
+    newLoss = criterion(y_pred_rl, clean)
+    
+    return newLoss
+    
 
 class DNN_RL(nn.Module):
     def __init__(self, l1_2=None):
@@ -172,28 +203,8 @@ def MMSE_pretrain(chunk_size, x_path, y_path, model_path, cluster_path,
                 audio = audio.to(device)
                 target = target.to(device)
                 output = l1(audio)
-
-                phase = pad(np.load(imag_path+fnames[step]), maxlen).T
-
-                Q_pred = output.detach().cpu().numpy()
-
-                wiener_rl = np.zeros((1339, 257))
-
-                #Select template index, predict Wiener filter
-                for i, row in enumerate(Q_pred):
-                    ind = np.argmax(row)
-                    G_k_pred = G[ind]
-                    wiener_rl[i] = G_k_pred
-
-
-                x_source = np.load(x_path+fnames[step])
-                x_source = pad(x_source, maxlen).T
-                y_pred_rl = np.multiply(x_source, wiener_rl) + phase
-                y_pred_rl = torch.tensor(y_pred_rl, requires_grad=True).cuda().float()
-
-                clean = pad(np.load(clean_path+fnames[step]), maxlen).T
-                clean = torch.tensor(clean).cuda().float()
-                newLoss = criterion(y_pred_rl, clean)                
+                
+                newLoss = q_training_step(output, x_path, clean_path, imag_path, fnames)               
                 chunk_loss += newLoss.data
                 optimizer.zero_grad()
                 newLoss.backward()
