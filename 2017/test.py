d0518ecbd30667feaa49e79eb1eb5be61be91ae3
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-08-28 12:41:30 -0400

    debug

--- 2017/test.py
+++ 2017/test.py
@@ -49,7 +49,7 @@ q_learning(x_path='/nobackup/anakuzne/data/snr0_train/',
            model_path='/nobackup/anakuzne/data/model_wiener_50/',
            clean_path='/nobackup/anakuzne/data/snr0_train_clean/')
 '''
-'''
+
 MMSE_pretrain(chunk_size=1000, x_path='/nobackup/anakuzne/data/snr0_train/', 
            y_path='/nobackup/anakuzne/data/snr0_w_target/', 
            model_path='/nobackup/anakuzne/data/qfunc_pretrain/',
@@ -61,3 +61,4 @@ MMSE_train(chunk_size=1000, x_path='/nobackup/anakuzne/data/snr0_train/',
            model_path='/nobackup/anakuzne/data/qfunc_pretrain/',
            cluster_path = '/nobackup/anakuzne/data/kmeans_centers.npy',
            clean_path = '/nobackup/anakuzne/data/snr0_train_clean/')
+'''
