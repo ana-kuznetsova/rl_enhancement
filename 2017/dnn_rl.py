ca2f92c2aad25a1fb8c2a5f2a42cf2fda2781819
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-09-07 12:56:05 -0400

    debug

--- 2017/dnn_rl.py
+++ 2017/dnn_rl.py
@@ -130,7 +130,7 @@ class MMSE_loss(torch.nn.Module):
                 sums.append(diff)
             sums = np.asarray(sums)
             A_t.append(np.argmin(sums))
-        A_t = np.asarray(A_t).reshape(-1, 1)
+        A_t = np.asarray(A_t)
 
         #Generate predicted q_func
 
