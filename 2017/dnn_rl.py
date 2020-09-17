b4cf9e38d1203263aafc26a4e7957bf58294d3ca
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-09-17 12:02:24 -0400

    fix reward

--- 2017/dnn_rl.py
+++ 2017/dnn_rl.py
@@ -477,11 +477,11 @@ def q_learning(num_episodes, x_path, cluster_path, model_path, clean_path,
 
         x_source_wav = invert(x_source)
         y_map_wav = invert(y_pred_map)[:x_source_wav.shape[0]]
-        y_rl_wav = invert(y_pred_map)[:x_source_wav.shape[0]]
+        y_rl_wav = invert(y_pred_rl)[:x_source_wav.shape[0]]
         
         z_rl = calc_Z(x_source_wav, y_rl_wav)
         z_map = calc_Z(x_source_wav, y_map_wav)
-        #print('Z-scores:', z_rl, z_map)
+        print('Z-scores:', z_rl, z_map)
 
         clean = np.load(clean_path+x_name)
         E = time_weight(y_pred_rl, pad(clean, maxlen))
