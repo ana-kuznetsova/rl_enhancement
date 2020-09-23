e6d786ec639a9c0eeb07b1740365f73a1718480f
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-09-23 11:44:53 -0400

    test

--- 2017/test.py
+++ 2017/test.py
@@ -16,7 +16,7 @@ from dnn_rl import MMSE_pretrain
 from dnn_rl import MMSE_train
 
 
-MMSE_pretrain(chunk_size=100, x_path='/N/slate/anakuzne/se_data/snr0_train/', 
+MMSE_pretrain(chunk_size=1000, x_path='/N/slate/anakuzne/se_data/snr0_train/', 
            a_path='/N/slate/anakuzne/se_data/action_labels/',
            model_path='/N/slate/anakuzne/se_data/qfunc_pretrain/',
            cluster_path = '/N/slate/anakuzne/se_data/kmeans_centers.npy',
