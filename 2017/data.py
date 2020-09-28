c5616baf226ff48710b01a0cdc2d6ae53af5cd7f
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-09-28 17:40:23 -0400

    debug

--- 2017/data.py
+++ 2017/data.py
@@ -255,7 +255,7 @@ def get_freq_bins(train_paths, ind, maxlen=1339):
         #f = STFT(f, 512, 256)
         
         try:
-            f = np.load(path)
+            f = np.load(path).T
         except ValueError:
             print(f)
         #f = pad(f, maxlen).T
