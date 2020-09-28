cf105369382ea93c5b95ac54a346671feabf4dcf
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-09-28 17:15:26 -0400

    data

--- 2017/main.py
+++ 2017/main.py
@@ -42,9 +42,9 @@ def main(args):
         FS = 16000
         noise_path = '/N/project/aspire_research_cs/Data/Corpora/Noise/cafe_16k.wav'
         
-        #makeMelSpecs(args.x_path, args.y_path)
+        makeMelSpecs(args.x_path, args.y_path)
 
-        
+        '''
         calc_MMSE_labels(
             x_path='/N/slate/anakuzne/se_data/snr0_train_melspecs/',
             a_path='/N/slate/anakuzne/se_data/action_labels/',
@@ -52,7 +52,7 @@ def main(args):
             cluster_path = '/N/slate/anakuzne/se_data/kmeans_centers.npy'
         )
 
-        '''
+        
         print('Generating TRAINING data...')
         train_files = collect_paths('/u/anakuzne/data/TIMIT_full/train/')
         out_path = '/u/anakuzne/data/snr0_train/'
