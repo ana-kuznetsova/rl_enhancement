83e5e742765f77af1097a0d7a07a975cfc728fb8
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-10-06 13:10:21 -0400

    debug

--- 2017/dnn_rl.py
+++ 2017/dnn_rl.py
@@ -220,7 +220,7 @@ def MMSE_pretrain(chunk_size, x_path, a_path, model_path, cluster_path,
 
             for x, target in trainData:
                 x = x.to(device)
-                print(x.view(-1, 1))
+                print(x.view(-1, 1).shape)
                 target = target.to(device).long()
                 target = torch.flatten(target)
                 output = l1(x)
