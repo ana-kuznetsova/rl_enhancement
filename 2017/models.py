9f67e85cffa7009bfc3e54d1dc5c5d07fa890002
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-10-07 12:27:28 -0400

    debug

--- 2017/models.py
+++ 2017/models.py
@@ -184,8 +184,9 @@ def pretrain(chunk_size, model_path, x_path, y_path, loss_path, num_epochs=50
             for x, target in loader:
                 x = x.to(device)
                 x = x.reshape(x.shape[1], x.shape[2])
+                print('X:', x.shape)
                 target = target.to(device).long()
-                target = torch.flatten(target)
+                print('Y:', target.shape)
                 output = l1(x)
 
                 newLoss = criterion(output, target)              
