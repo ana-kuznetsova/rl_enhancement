762f5ac56b5b061da72f244e4cf4b651eb4ff426
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-10-14 11:03:41 -0400

    add resume training

--- 2017/main.py
+++ 2017/main.py
@@ -83,7 +83,7 @@ def main(args):
         '''
 
     elif args.mode=='pretrain':
-        pretrain(args.chunk_size, args.model_path, args.x_path, args.y_path, args.resume)
+        pretrain(args.chunk_size, args.model_path, args.x_path, args.y_path, resume=args.resume)
 
     elif args.mode=='eval':
         eval_pesq(args.preds_path, args.y_path, args.test_path, args.test_out, args.imag)
