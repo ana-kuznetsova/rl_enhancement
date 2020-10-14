49fc7bbc957012816febf41a02f5ee94761f8501
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-10-14 14:29:36 -0400

    debug

--- 2017/dnn_rl.py
+++ 2017/dnn_rl.py
@@ -228,7 +228,7 @@ def MMSE_pretrain(chunk_size, x_path, a_path, model_path, cluster_path,
                     pred_qfunc = output.detach().cpu().numpy()
                     print(pred_qfunc.shape)
                     for i in range(pred_qfunc.shape[1]):
-                        print(pred_qfunc[:, i])
+                        n = pred_qfunc[:, i]
 
 
                 chunk_loss = (chunk_loss.detach().cpu().numpy())/len(X_chunk)
