a034ea1572016e7170ebafba3a09f3dcf5bc0d59
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-11-02 13:22:12 -0500

    debug

--- 2017/data.py
+++ 2017/data.py
@@ -206,6 +206,7 @@ def make_windows(x_path, a_path, ind, P, win_len, hop_size, fs, nn_type='qfunc')
             true_a = np.load(a_path+path.split('_')[0]+'.npy').reshape(-1,1)
         elif nn_type=='map':
             #true_a = np.log(np.load(a_path+path)).T
+            print("PATH:", path)
             true_a = np.log(np.load(a_path+path.split('_')[0]+'.npy')).T
         arr = get_X_batch(arr, P)
 
