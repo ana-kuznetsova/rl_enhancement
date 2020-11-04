17e90f3cc4252970dc172eaee160cd6401a42c0d
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-11-04 14:29:10 -0500

    debug

--- 2017/qlearn.py
+++ 2017/qlearn.py
@@ -109,9 +109,9 @@ def q_learning(num_episodes, x_path, cluster_path, model_path, clean_path,
         x_source_wav = read(clean_path+x_name.split('_')[0]+'.wav')
         print("wav source", x_source_wav.shape)
         print("dnn", y_pred_dnn.shape)
-        #y_map_wav = torch.istft(y_pred_dnn, n_fft=257, hop_length=256, win_length=512)
+        #y_map_wav = torch.istft(y_pred_dnn, n_fft=512, hop_length=256)
         #print("DNN-pred:", y_map_wav.shape)
-        y_rl_wav = InverseMelScale(y_pred_rl, n_fft=512, hop_length=356, n_mels=64)
+        y_rl_wav = InverseMelScale(y_pred_rl, n_stft=512, hop_length=356, n_mels=64)
         print("RL out:", y_rl_wav.shape)
         
         z_rl = calc_Z(x_source_wav, y_rl_wav)
