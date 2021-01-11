c3f57127f437c9a336ddfdde9e210f8201ab19c2
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2021-01-11 18:48:55 -0500

    padding

--- 2020/model.py
+++ 2020/model.py
@@ -9,7 +9,7 @@ from preproc import get_feats
 class Actor(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv2d1 = nn.Conv2d(in_channels=3, out_channels=30,
+        self.conv2d1 = nn.Conv2d(in_channels=1, out_channels=30,
                                  kernel_size=(5, 15), stride=(1,1), 
                                  padding=(2,7))
         self.conv2d2 = nn.Conv2d(in_channels=30, out_channels=60,
@@ -30,6 +30,6 @@ dataset = DataLoader('/nobackup/anakuzne/data/voicebank-demand/clean_trainset_28
 loader = data.DataLoader(dataset, batch_size=10, shuffle=True)
 
 for batch in loader:
-    x = batch["noisy"]
-    t = batch["clean"]
+    x = batch["noisy"].unsqueeze(1)
+    t = batch["clean"].unsqueeze(1)
     print("Noisy:", x.shape)
