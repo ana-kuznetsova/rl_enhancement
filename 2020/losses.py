af84462226f07cd699f1de824b620d858cba9535
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2021-01-12 01:27:43 -0500

    debug

--- 2020/losses.py
+++ 2020/losses.py
@@ -16,7 +16,7 @@ class SDRLoss(nn.Module):
     def forward(self, t, y):
         temp = []
         for i in range(len(t)):
-            print(t[i], y[i])
+            print(self.norm2(t[i]), self.norm2((t[i]-y[i])))
             frac = self.norm2(t[i])/self.norm2((t[i]-y[i]))
             val = 10*torch.log10(frac)
             val = self.clip(val)
