6ea7904e219bcc9dbd33bd901294cd98a795dc8f
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2021-01-11 22:35:13 -0500

    debug

--- 2020/preproc.py
+++ 2020/preproc.py
@@ -31,13 +31,11 @@ def get_feats(clean_path, noisy_path, maxlen=1890):
     stft = torchaudio.transforms.Spectrogram(n_fft=1024, win_length=512, hop_length=128)
     clean = stft(torch.tensor(clean))
     noisy = stft(torch.tensor(noisy))
-
-    pad_func = nn.ZeroPad2d(padding=(0, maxlen-noisy.shape[1], 0, 0))
     
     mask = torch.ones(1, clean.shape[1])
-    mask = pad_func(mask)
-    clean = pad_func(clean)
-    noisy = pad_func(noisy)
+    mask = nn.ZeroPad2d(padding=(0, maxlen-clean.shape[1], 0, 0))(mask)
+    clean = nn.ZeroPad2d(padding=(0, maxlen-clean.shape[1], 0, 0))(clean)
+    noisy = nn.ZeroPad2d(padding=(0, maxlen-noisy.shape[1], 0, 0))(noisy)
     print(clean.shape, noisy.shape, mask.shape)
     return {"clean":clean, "noisy":noisy, "mask":mask}
 
