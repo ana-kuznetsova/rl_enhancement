b5ee850597e48e1fac920e7ac457384b9d8dac32
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2021-01-15 12:37:55 -0500

    train

--- 2020/train.py
+++ 2020/train.py
@@ -1,2 +1,5 @@
-    #dataset = DataLoader('/nobackup/anakuzne/data/voicebank-demand/clean_trainset_28spk_wav/',
-    #                    '/nobackup/anakuzne/data/voicebank-demand/noisy_trainset_28spk_wav/', get_feats)
+from model import pretrain_actor
+
+pretrain_actor('/nobackup/anakuzne/data/voicebank-demand/clean_trainset_28spk_wav/',
+                '/nobackup/anakuzne/data/voicebank-demand/noisy_trainset_28spk_wav/', 
+                '/nobackup/anakuzne/data/experiments/speech_enhancement/2020/pre_actor/', 290)
