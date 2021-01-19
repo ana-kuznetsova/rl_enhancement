9af06dddb88b9587c91ae34b8cb5b572ae561b3e
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2021-01-19 14:36:47 -0500

    dev

--- 2020/metric_gan.py
+++ 2020/metric_gan.py
@@ -36,17 +36,13 @@ class Discriminator(nn.Module):
     def __init__(self):
         super().__init__()
         self.conv2d1 = nn.Conv2d(in_channels=1, out_channels=15,
-                                 kernel_size=(5, 5),stride=(1,1), 
-                                 padding=(2,7))
+                                 kernel_size=(5, 5))
         self.conv2d2 = nn.Conv2d(in_channels=15, out_channels=25,
-                                 kernel_size=(7, 7),stride=(1,1), 
-                                 padding=(2,7))
+                                 kernel_size=(7, 7))
         self.conv2d3 = nn.Conv2d(in_channels=25, out_channels=40,
-                                 kernel_size=(9, 9), stride=(1,1), 
-                                 padding=(2,7))
+                                 kernel_size=(9, 9))
         self.conv2d4 = nn.Conv2d(in_channels=40, out_channels=50,
-                                 kernel_size=(11, 11), stride=(1,1), 
-                                 padding=(2,7))
+                                 kernel_size=(11, 11))
         self.avg_pool = nn.AvgPool2d(kernel_size=(50,50))
         self.leaky_relu = nn.LeakyReLU()
         #self.fc1 = nn.Linear()
@@ -55,6 +51,7 @@ class Discriminator(nn.Module):
         x = self.conv2d2(x)
         x = self.conv2d3(x)
         x = self.conv2d4(x)
+        x = self.avg_pool(x)
         return x
 
 
