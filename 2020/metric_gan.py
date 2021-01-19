83c0e4c097ebb3468bd1c668ba051a6fd5c85dce
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2021-01-18 22:19:34 -0500

    dev

--- 2020/metric_gan.py
+++ 2020/metric_gan.py
@@ -53,5 +53,5 @@ for i, batch in enumerate(loader):
     out_r = torch.transpose(out_r, 1, 2)
     out_i = torch.transpose(out_i, 1, 2)
     y = predict(x.squeeze(1), (out_r, out_i))
-    y = critic(y)
+    y = critic(y.unsqueeze(1))
     print(y)
