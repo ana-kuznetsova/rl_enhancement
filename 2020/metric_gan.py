5373939ce59a736937e54e1914ca3003300391e8
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2021-01-18 22:07:34 -0500

    add actor-critic

--- 2020/metric_gan.py
+++ 2020/metric_gan.py
@@ -38,7 +38,7 @@ critic.cuda()
 critic = critic.to(device)
 
 actor = Actor()
-actor.load_state_dict(torch.load('/nobackup/anakuzne/data/experiments/speech_enhancement/2020/pre_actor/'))
+actor.load_state_dict(torch.load('/nobackup/anakuzne/data/experiments/speech_enhancement/2020/pre_actor/actor_best.pth'))
 actor = actor.to(device)
 
 dataset = DataLoader('/nobackup/anakuzne/data/voicebank-demand/clean_trainset_28spk_wav/',
