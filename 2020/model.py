77ac49dd625f4a6b12f7d49a9417a50fc1aa4e43
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2021-01-21 17:28:26 -0500

    dev

--- 2020/model.py
+++ 2020/model.py
@@ -146,9 +146,9 @@ def pretrain_critic():
         out_r = torch.transpose(out_r, 1, 2)
         out_i = torch.transpose(out_i, 1, 2)
         y = predict(x.squeeze(1), (out_r, out_i), floor=True)
-        y = torch.transpose(y, 1, 2)
-        print(y.shape, t.shape)
-        #y_gen = critic(y)
+        t = t.unsqueeze(1)
+        disc_input = torch.cat((y, t), 2)
+        print(disc_input.shape)
 
 def pretrain_actor(clean_path, noisy_path, model_path, num_epochs):
 
