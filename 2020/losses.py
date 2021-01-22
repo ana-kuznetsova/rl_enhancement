6bd79a0e3357afc874e0ee02e7dce9393003cb7e
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2021-01-21 19:01:13 -0500

    dev

--- 2020/losses.py
+++ 2020/losses.py
@@ -36,7 +36,7 @@ class CriticLoss(nn.Module):
             x = x[i][:, :ind]
             y = y[i][:, :ind]
             s = s[i][:, :ind]
-            print(x.shape, y.shape, s.shape)
+            #print(x.shape, y.shape, s.shape)
             x = torch.stft(x, n_fft=1024, win_length=512, hop_length=128, 
                            normalized=True, return_complex=True)
             y = torch.stft(y, n_fft=1024, win_length=512, hop_length=128, 
@@ -63,3 +63,4 @@ class CriticLoss(nn.Module):
 
         for i in range(x.shape[0]):
             true_scores = self.calc_true_pesq(x, y, s, mask)
+            print(true_scores)
