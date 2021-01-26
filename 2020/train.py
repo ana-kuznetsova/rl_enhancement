9da8106a4454bd278f49e92734736cba8f6523b2
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2021-01-26 11:22:16 -0500

    critic pretrain

--- 2020/train.py
+++ 2020/train.py
@@ -1,16 +1,18 @@
 from model import pretrain_actor, pretrain_critic
 from model import inference
 
-
+'''
 pretrain_actor('/nobackup/anakuzne/data/voicebank-demand/clean_trainset_28spk_wav/',
                 '/nobackup/anakuzne/data/voicebank-demand/noisy_trainset_28spk_wav/', 
                 '/nobackup/anakuzne/data/experiments/speech_enhancement/2020/pre_actor/', 290)
 
-'''
+
 inference('/nobackup/anakuzne/data/voicebank-demand/clean_trainset_28spk_wav/',
           '/nobackup/anakuzne/data/voicebank-demand/noisy_trainset_28spk_wav/', 
           '/nobackup/anakuzne/data/experiments/speech_enhancement/2020/pre_actor/actor_best.pth')
 
-
-pretrain_critic()
 '''
+
+pretrain_critic('/nobackup/anakuzne/data/voicebank-demand/clean_trainset_28spk_wav/', 
+                '/nobackup/anakuzne/data/voicebank-demand/noisy_trainset_28spk_wav/',
+                '/nobackup/anakuzne/data/experiments/speech_enhancement/2020/pre_critic/', 200)
