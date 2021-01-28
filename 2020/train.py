b484d377bec92ce46534c9978b4746fc7669d97e
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2021-01-28 14:15:10 -0500

    run test

--- 2020/train.py
+++ 2020/train.py
@@ -6,13 +6,11 @@ pretrain_actor('/nobackup/anakuzne/data/voicebank-demand/clean_trainset_28spk_wa
                 '/nobackup/anakuzne/data/voicebank-demand/noisy_trainset_28spk_wav/', 
                 '/nobackup/anakuzne/data/experiments/speech_enhancement/2020/pre_actor/', 290)
 
-
-inference('/nobackup/anakuzne/data/voicebank-demand/clean_trainset_28spk_wav/',
-          '/nobackup/anakuzne/data/voicebank-demand/noisy_trainset_28spk_wav/', 
-          '/nobackup/anakuzne/data/experiments/speech_enhancement/2020/pre_actor/actor_best.pth')
-'''
-
 pretrain_critic('/nobackup/anakuzne/data/voicebank-demand/clean_trainset_28spk_wav/', 
                 '/nobackup/anakuzne/data/voicebank-demand/noisy_trainset_28spk_wav/',
                 '/nobackup/anakuzne/data/experiments/speech_enhancement/2020/pre_critic/', 200)
+'''
+inference('/nobackup/anakuzne/data/voicebank-demand/clean_testset_wav/',
+          '/nobackup/anakuzne/data/voicebank-demand/noisy_testset_wav/', 
+          '/nobackup/anakuzne/data/experiments/speech_enhancement/2020/pre_actor/actor_best.pth')
 
