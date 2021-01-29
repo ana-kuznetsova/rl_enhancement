0ffe35ae1efc0ed3aa2a0dbf3f627ce261b63521
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2021-01-29 10:58:07 -0500

    switch gpu

--- 2020/model.py
+++ 2020/model.py
@@ -167,14 +167,14 @@ def train_GAN(clean_path, noisy_path, actor_path, critic_path, model_path, num_i
 
 def pretrain_critic(clean_path, noisy_path, model_path, num_epochs):
 
-    device = torch.device("cuda")
+    #device = torch.device("cuda")
     actor = Actor()
     actor = nn.DataParallel(actor, device_ids=[1, 2])
     actor.load_state_dict(torch.load('/nobackup/anakuzne/data/experiments/speech_enhancement/2020/pre_actor/actor_best.pth'))
-    actor = actor.to(device)
+    #actor = actor.to(device)
 
     critic = Critic()
-    critic = critic.to(device)
+    #critic = critic.to(device)
     critic.apply(init_weights)
     critic = nn.DataParallel(critic, device_ids=[1, 2])
 
