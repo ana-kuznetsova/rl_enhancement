34e84591272a78cbd4b88e24597a453fbd832c6c
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2021-01-29 11:02:53 -0500

    debug

--- 2020/model.py
+++ 2020/model.py
@@ -171,13 +171,13 @@ def pretrain_critic(clean_path, noisy_path, model_path, num_epochs):
     actor = Actor()
     actor = nn.DataParallel(actor, device_ids=[1, 2])
     actor.load_state_dict(torch.load('/nobackup/anakuzne/data/experiments/speech_enhancement/2020/pre_actor/actor_best.pth'))
-    #actor = actor.to(device)
+    actor = actor.cuda()
+    print(next(actor.parameters()).is_cuda)
 
     critic = Critic()
     #critic = critic.to(device)
     critic.apply(init_weights)
     critic = nn.DataParallel(critic, device_ids=[1, 2])
-    print(critic.device)
 
     criterion = CriticLoss()
     criterion.cuda()
