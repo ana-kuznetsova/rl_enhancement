bb95c1f1428d4ab9681082257aaeebe22edf07bb
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2021-01-29 11:38:58 -0500

    debug

--- 2020/model.py
+++ 2020/model.py
@@ -167,7 +167,7 @@ def train_GAN(clean_path, noisy_path, actor_path, critic_path, model_path, num_i
 
 def pretrain_critic(clean_path, noisy_path, model_path, num_epochs):
 
-    device = torch.device("cuda")
+    device = torch.device("cuda:1")
     actor = Actor()
     actor = nn.DataParallel(actor, device_ids=[1, 2])
     actor.load_state_dict(torch.load('/nobackup/anakuzne/data/experiments/speech_enhancement/2020/pre_actor/actor_best.pth'))
