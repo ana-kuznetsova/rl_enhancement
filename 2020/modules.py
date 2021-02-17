8c567e98b63336d9d470950e3800f569b0a507c7
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2021-02-17 16:15:12 -0500

    debug

--- 2020/modules.py
+++ 2020/modules.py
@@ -266,7 +266,7 @@ def pretrain_actor(clean_path, noisy_path, model_path, num_epochs):
     model.cuda()
     model = model.to(device)
     #model.apply(init_weights)
-    model = nn.DataParallel(model, device_ids=[1, 2])
+    model = nn.DataParallel(model, device_ids=[1, 3])
 
     criterion = SDRLoss()
     criterion.cuda()
