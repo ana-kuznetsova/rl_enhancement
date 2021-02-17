16f4a8b1d8a2b4e3ff4de8163f03fa685b473298
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2021-02-17 16:18:01 -0500

    debug

--- 2020/modules.py
+++ 2020/modules.py
@@ -33,10 +33,11 @@ class Actor(nn.Module):
         self.bi_lstm = nn.LSTM(512, hidden_size=512, num_layers=2, 
                                batch_first=True, dropout=0.3, bidirectional=True)
         self.linear2 = nn.Linear(1024, 257*2)
+        self.bnorm = nn.BatchNorm2d(1)
 
     def forward(self, x):
         x = x.real
-        print("X:", x.shape)
+        x = self.bnorm(x)
         x = self.conv2d1(x)
         x = self.conv2d2(x)
         x = self.conv2d3(x)
