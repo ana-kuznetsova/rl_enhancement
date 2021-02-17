d3521f41bd84bcf552f4c912c3d49c4d11e901a1
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2021-02-17 14:29:39 -0500

    debug

--- 2020/modules.py
+++ 2020/modules.py
@@ -303,6 +303,7 @@ def pretrain_actor(clean_path, noisy_path, model_path, num_epochs):
             x = batch["noisy"].unsqueeze(1).to(device)
             t = batch["clean"].unsqueeze(1).to(device)
             m = batch["mask"].to(device)
+            print("X, T:", x[0], t[0])
             out_r, out_i = model(x)
             out_r = torch.transpose(out_r, 1, 2)
             out_i = torch.transpose(out_i, 1, 2)
