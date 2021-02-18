9c7b9fc284a1449cf23f464ef4b7afbe94e00e99
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2021-02-18 15:24:08 -0500

    debug

--- 2020/modules.py
+++ 2020/modules.py
@@ -37,6 +37,9 @@ class Actor(nn.Module):
 
     def forward(self, x):
         x = 10*torch.log10(x.abs())
+        #-inf is caused by zero padding
+        #Change inf to zeros
+        x[x==float("-Inf")] = 0
         print("X:", x[:5, :1])
         x = self.conv2d1(x)
         x = self.conv2d2(x)
