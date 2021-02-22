329e53249813d73022ace4b2ad3243237fc0075b
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2021-02-22 12:21:58 -0500

    inference

--- 2020/train.py
+++ 2020/train.py
@@ -8,17 +8,18 @@ pretrain_critic('/nobackup/anakuzne/data/voicebank-demand/clean_trainset_28spk_w
                 '/nobackup/anakuzne/data/experiments/speech_enhancement/2020/pre_critic/', 200)
 
 
-inference_actor('/nobackup/anakuzne/data/voicebank-demand/clean_testset_wav/',
-          '/nobackup/anakuzne/data/voicebank-demand/noisy_testset_wav/', 
-          '/nobackup/anakuzne/data/experiments/speech_enhancement/2020/pre_actor/actor_best.pth', 
-          '/nobackup/anakuzne/data/experiments/speech_enhancement/2020/pre_actor_test/')
-
 pretrain_critic('/nobackup/anakuzne/data/voicebank-demand/clean_trainset_28spk_wav/', 
                 '/nobackup/anakuzne/data/voicebank-demand/noisy_trainset_28spk_wav/',
                 '/nobackup/anakuzne/data/experiments/speech_enhancement/2020/pre_critic_1/', 200)
-'''
+
 
 
 pretrain_actor('/nobackup/anakuzne/data/voicebank-demand/clean_trainset_28spk_wav/',
                 '/nobackup/anakuzne/data/voicebank-demand/noisy_trainset_28spk_wav/', 
                 '/nobackup/anakuzne/data/experiments/speech_enhancement/2020/pre_actor_1/', 290)
+'''
+
+inference_actor('/nobackup/anakuzne/data/voicebank-demand/clean_testset_wav/',
+          '/nobackup/anakuzne/data/voicebank-demand/noisy_testset_wav/', 
+          '/nobackup/anakuzne/data/experiments/speech_enhancement/2020/pre_actor_1/actor_best.pth', 
+          '/nobackup/anakuzne/data/experiments/speech_enhancement/2020/pre_actor_test/')
