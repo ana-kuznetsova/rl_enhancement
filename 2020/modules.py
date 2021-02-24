fix gradient flow

2020/modules.py
@@ -272,6 +272,7 @@ def pretrain_actor(clean_path, noisy_path, model_path, num_epochs):
 
         dataset = Data(clean_path, noisy_path, 1000)
         loader = data.DataLoader(dataset, batch_size=5, shuffle=True, collate_fn=collate_custom)
+        model.train()
 
         for batch in loader:
             x = batch["noisy"].unsqueeze(1).to(device)
@@ -303,12 +304,15 @@ def pretrain_actor(clean_path, noisy_path, model_path, num_epochs):
 
             dataset = Data(clean_path, noisy_path, 1000)
             loader = data.DataLoader(dataset, batch_size=5, shuffle=True, collate_fn=collate_custom)
+            
+            model.eval()
 
             for batch in loader:
                 x = batch["noisy"].unsqueeze(1).to(device)
                 t = batch["clean"].unsqueeze(1).to(device)
                 m = batch["mask"].to(device)
-                out_r, out_i = model(x)
+                with torch.no_grad():
+                    out_r, out_i = model(x)
                 out_r = torch.transpose(out_r, 1, 2)
                 out_i = torch.transpose(out_i, 1, 2)
                 y = predict(x.squeeze(1), (out_r, out_i))