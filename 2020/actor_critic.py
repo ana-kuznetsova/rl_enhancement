405b7df427bf2d223610c1be3671549aeff0eddc
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2021-03-02 17:37:16 -0500

    debug

--- 2020/actor_critic.py
+++ 2020/actor_critic.py
@@ -94,7 +94,7 @@ def calc_metrics(loader, actor, device):
         y = predict(x.squeeze(1), (out_r, out_i))
         t = t.squeeze()
         m = m.squeeze()
-        print("Y:", y.shape)
+        #print("Y:", y.shape)
         #source, targets, preds = inverse(t, y, m, x)
         targets, preds = inverse(t, y, m, x)
 
