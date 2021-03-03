bbe000f2d84bb6221bd9f36baedfaeee159de92d
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2021-03-02 22:15:54 -0500

    debug

--- 2020/modules.py
+++ 2020/modules.py
@@ -54,6 +54,7 @@ class Actor(nn.Module):
         #-inf is caused by zero padding
         #Change inf to zeros
         x[x==float("-Inf")] = 0
+        print(x.shape)
         x = self.conv2d1(x)
         print(x.shape)
         x = self.conv2d2(x)
