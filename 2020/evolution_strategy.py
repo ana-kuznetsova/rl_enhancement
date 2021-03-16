5e816d999941efb0b5b3489549c3f456caee8df3
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2021-03-16 15:38:53 -0400

    add validation

--- 2020/evolution_strategy.py
+++ 2020/evolution_strategy.py
@@ -81,8 +81,9 @@ def train(clean_path, noisy_path, model_path, num_epochs, elite_size=200, popula
             print("Validation...")
             dataset = Data(clean_path, noisy_path, val_size)
             loader = data.DataLoader(dataset, batch_size=16, shuffle=False, collate_fn=collate_custom)
+            print("Steps:", len(loader))
+            
             for i, batch in enumerate(loader):
-                print("Steps:", len(loader))
                 x = batch["noisy"].unsqueeze(1).to(device)
                 t = batch["clean"].unsqueeze(1).to(device)
                 out_r, out_i = model(x)
