eb24d0ada5f0b51cb14758b67289073b939eedbe
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2021-04-06 17:37:45 -0400

    debug

--- 2020/modules.py
+++ 2020/modules.py
@@ -246,7 +246,7 @@ def pretrain_actor(clean_path, noisy_path, model_path, num_epochs):
 
     device = torch.device("cuda:2")
     model = Actor()
-    model = nn.DataParallel(model, device_ids=[2, 3])
+    #model = nn.DataParallel(model, device_ids=[2, 3])
     model = model.to(device)
     model.apply(init_weights)
     
